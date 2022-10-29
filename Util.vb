9702bfb3e07ce90364db9159db2b9578ea1df531
admiralnelson <admiralofinternetmeme@outlook.com>
2022-10-29 14:35:54 +0700

    everything is ok

--- Util.vb
+++ Util.vb
@@ -1,8 +1,16 @@
 ﻿Imports TaleWorlds.Library
+Imports TaleWorlds.MountAndBlade
 
 Public Module Util
     Public Sub Print(s As String, Optional colour As Color = Nothing)
         If IsNothing(colour) Then colour = Color.White
         InformationManager.DisplayMessage(New InformationMessage(s, colour))
     End Sub
+
+    Public Function IsAttackingHuman(combatlog As CombatLogData) As Boolean
+        If Not combatlog.IsVictimAgentHuman Then
+            Return combatlog.DoesVictimAgentHaveRiderAgent AndAlso combatlog.IsVictimAgentRiderAgentMine
+        End If
+        Return combatlog.IsVictimAgentMine
+    End Function
 End Module
--- WarbandFeedPatch.vb
+++ WarbandFeedPatch.vb
@@ -9,32 +9,37 @@ Public Module StringConstants
     Public ReadOnly KnockedUnconciouBysStr As New TextObject("{=admiralnelson_knocked_unconscious}{agent1} knocked unconscious by {agent2}")
     Public ReadOnly KilledByStr As New TextObject("{=admiralnelson_killed_by}{agent1} killed by {agent2}")
     Public ReadOnly DamageDeliveredStr As New TextObject("{=admiralnelson_delivered_damage}Delivered {amount} damage")
+    Public ReadOnly DamageReceivedStr As New TextObject("{=admiralnelson_received_damage}Received {amount} damage")
     Public ReadOnly CouchedDamageStr As New TextObject("{=admiralnelson_delivered_couch_damage}Delivered {amount} couched lance damage!")
     Public ReadOnly FriendlyFireStr As New TextObject("{=admiralnelson_friendly_fire}Friendly Fire!")
     Public ReadOnly HeadShotStr As New TextObject("{=admiralnelson_friendly_fire}Headshot!")
+    Public ReadOnly ChargedMountStr As New TextObject("{=admiralnelson_charged_mount}Mount charged for {amount} damage")
+    Public ReadOnly SpeedBonusStr As New TextObject("{=admiralnelson_speed_bonus}Relative movement speed: {amount} m/s")
+    Public ReadOnly HitDistanceStr As New TextObject("{=admiralnelson_hit_distance}Distance: {amount} m")
 End Module
 Public Module Colours
     Public ReadOnly ColourUnconciousOurSide = Color.ConvertStringToColor("#FFA862FF")
-    Public ReadOnly ColourDeathOurSide = Color.ConvertStringToColor("#9AD7B4FF")
+    Public ReadOnly ColourDeathOurSide = Color.ConvertStringToColor("#AF6353FF")
     Public ReadOnly ColourUnconciousAllySide = Color.ConvertStringToColor("#EEA4FFFF")
     Public ReadOnly ColourDeathAllySide = Color.ConvertStringToColor("#F11CB5FF")
     Public ReadOnly ColourKill = Color.ConvertStringToColor("#9AD7B4FF")
     Public ReadOnly ColourRed = Color.ConvertStringToColor("#FF0000FF")
     Public ReadOnly ColourGold = Color.ConvertStringToColor("#FFD700FF")
+    Public ReadOnly ColourDamage = Color.ConvertStringToColor("#FFAAAAFF")
     Public Function GetColour(theAgent As Agent, isDead As Boolean) As Color
         If isDead Then
-            If theAgent.Team.IsPlayerAlly Then
-                Return ColourDeathAllySide
-            End If
             If theAgent.Team.IsPlayerTeam Then
                 Return ColourDeathOurSide
             End If
-        Else
             If theAgent.Team.IsPlayerAlly Then
-                Return ColourUnconciousAllySide
+                Return ColourDeathAllySide
             End If
+        Else
             If theAgent.Team.IsPlayerTeam Then
                 Return ColourUnconciousOurSide
+            End If
+            If theAgent.Team.IsPlayerAlly Then
+                Return ColourUnconciousAllySide
             End If
         End If
         Return ColourKill
@@ -68,11 +73,52 @@ Public Class WarbandFeedPatch
                 Print(FriendlyFireStr.ToString(), ColourRed)
                 Return False
             End If
-            If isHeadshot Then
-                Print(HeadShotStr.ToString(), ColourGold)
-            End If
             Return False
         End Function
     End Class
+    <HarmonyPatch(GetType(SPKillFeedVM), "OnPersonalDamage")>
+    Public Class OnPersonalDamage
+        Public Shared Function Prefix(totalDamage As Integer, isVictimAgentMount As Boolean, isFriendlyFire As Boolean, victimAgentName As String) As Boolean
+            Return False
+        End Function
+    End Class
+
+
+    <HarmonyPatch(GetType(CombatLogManager), "GenerateCombatLog")>
+    Public Class GenerateCombatLog
+        Public Shared Function Prefix(logData As CombatLogData) As Boolean
+            If logData.TotalDamage <= 0 Then Return False
+            'this log is generated from bot POV, that why we check against human
+            If IsAttackingHuman(logData) Then
+                Dim str = DamageReceivedStr.SetTextVariable("amount", logData.TotalDamage - logData.AbsorbedDamage)
+                Print(str.ToString(), ColourDamage)
+                Return False
+            End If
+            If logData.IsAttackerAgentMount Then
+                If logData.TotalDamage > 0 Then
+                    Dim str = ChargedMountStr.SetTextVariable("amount", logData.TotalDamage - logData.AbsorbedDamage)
+                    Print(str.ToString(), Color.White)
+                End If
+            Else
+                If logData.TotalDamage > 0 Then
+                    Dim damstr = DamageDeliveredStr.SetTextVariable("amount", logData.TotalDamage - logData.AbsorbedDamage)
+                    Print(damstr.ToString(), Color.White)
+                End If
+                If logData.HitSpeed > 0.0001 AndAlso Not logData.IsRangedAttack Then
+                    Dim str = SpeedBonusStr.SetTextVariable("amount", logData.HitSpeed.ToString("N3"))
+                    Print(str.ToString(), Color.White)
+                ElseIf logData.IsRangedAttack Then
+                    Dim str = HitDistanceStr.SetTextVariable("amount", logData.HitSpeed.ToString("N3"))
+                    Print(str.ToString(), Color.White)
+                    If logData.IsFatalDamage Then
+                        Print(HeadShotStr.ToString(), ColourGold)
+                    End If
+                End If
+            End If
+
+            Return False
+        End Function
+    End Class
+
 
 End Class
